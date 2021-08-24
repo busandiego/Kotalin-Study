15507d3b70e5ae6e19704dce216927e28efaab9b
suin lee <silee@weedscomm.com>
2021-08-24 23:21:05 +0900

    loops

--- src/main/kotlin/org/kotlinlang/play/controlflow/Loops/Loops.kt
+++ src/main/kotlin/org/kotlinlang/play/controlflow/Loops/Loops.kt
@@ -1,2 +1,31 @@
 package org.kotlinlang.play.controlflow.Loops
 
+val coffees = listOf("carmel", "americano", "latte", "cappuccino")
+
+
+fun drinkCoffee() = println("drink a coffee")
+fun makeCoffee() = println("make a coffee")
+
+
+fun main() {
+
+    // Loops through each cake in the list.
+    for (coffee in coffees) {
+        println("i like $coffee coffee")
+    }
+
+    var coffeeDrunk: Int = 0
+    var coffeeMade: Int = 0
+
+    while (coffeeDrunk < 6) {
+        drinkCoffee()
+        coffeeDrunk++
+    }
+
+    do {
+        makeCoffee()
+        coffeeMade++
+    } while(coffeeMade < coffeeDrunk )
+    // while에서 조건에 안 맞으면 다시 do로 돌아감
+}
+
