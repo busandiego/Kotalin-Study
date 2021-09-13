b257d3db5ce89e237046afa9610b624f5fe10d94
suin lee <silee@weedscomm.com>
2021-09-14 00:02:47 +0900

    contatins

--- src/main/kotlin/org/kotlinlang/play/collections/Find.kt
+++ src/main/kotlin/org/kotlinlang/play/collections/Find.kt
@@ -13,11 +13,20 @@ fun main() {
     val first = words.find {it.startsWith("some")}
     val last = words.findLast { it.startsWith("some") }
 
+
     val nothing = words.find {it.contains("nothing")}
+
+    val firstContain = words.find { it.contains("some") }
+    val lastContain = words.findLast { it.contains("some") }
 
     println("The first word starting with \"some\" is \"$first\"")
     println("The last word starting with \"some\" is \"$last\"")
     println("The first word containing \"nothing\" is ${nothing?.let { "\"$it\"" } ?: "null"}")
+
+    println("The first word containing \"firstContain\" is $firstContain")
+    println("The last word containing \"lastContain\" is $lastContain")
+
+
 
 
 }
