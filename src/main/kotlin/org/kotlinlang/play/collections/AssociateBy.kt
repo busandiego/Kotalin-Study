6219c5512a37cd1accc447ca295003fcdeb6b507
suin lee <silee@weedscomm.com>
2021-09-27 22:32:51 +0900

    gradle error fix

--- src/main/kotlin/org/kotlinlang/play/collections/AssociateBy.kt
+++ src/main/kotlin/org/kotlinlang/play/collections/AssociateBy.kt
@@ -1,6 +1,19 @@
 package org.kotlinlang.play.collections
 
 // associateBy, groupBy
+// functions associateBy and groupBy build maps from the elemetns of a collection
+// indexed by the specified key.
+
+
+
+data class Person(val name: String, val city: String, val phone: String) // 1
 fun main() {
-//
+
+    val people = listOf(                                                     // 2
+        Person("John", "Boston", "+1-888-123456"),
+        Person("Sarah", "Munich", "+49-777-789123"),
+        Person("Svyatoslav", "Saint-Petersburg", "+7-999-456789"),
+        Person("Vasilisa", "Saint-Petersburg", "+7-999-123456"))
+
+
 }
